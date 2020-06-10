7ac54e13a4a22cba84cc33a6167da0935f232ea1
Shaun Luttin <admin@shaunluttin.com>
2020-06-10 09:33:37 -0700

    Add prettier to vim

--- vim/install_bundles.sh
+++ vim/install_bundles.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 git clone https://github.com/pangloss/vim-javascript.git ~/.vim/pack/vim-javascript/start/vim-javascript
+git clone https://github.com/prettier/vim-prettier ~/.vim/pack/plugins/start/vim-prettier
