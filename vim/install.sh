29824c828b0ca97cb651a3343e29c2120ea4b3dc
Shaun Luttin <admin@shaunluttin.com>
2020-06-10 09:53:32 -0700

    Replace README.md with install.sh

--- vim/install.sh
+++ vim/install.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+# Reset
+rm ~/.vimrc
+rm ~/.vim -rf
+
+# Install Settings
+cp ./_vimrc ~/.vimrc
+
+# Install Bundles
 git clone https://github.com/pangloss/vim-javascript.git ~/.vim/pack/vim-javascript/start/vim-javascript
 git clone https://github.com/prettier/vim-prettier ~/.vim/pack/plugins/start/vim-prettier
