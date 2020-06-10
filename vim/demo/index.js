832b042aab96512b2674c0617043afa53fe241f2
Shaun Luttin <admin@shaunluttin.com>
2020-06-10 09:31:03 -0700

    Add install_bundles.sh

---
+++ vim/demo/index.js
@@ -0,0 +1,7 @@
+
+export default () => {
+    const x = 10;
+    const y = 'ten';
+    const z = new Date();
+}
+
---
+++ vim/install_bundles.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+git clone https://github.com/pangloss/vim-javascript.git ~/.vim/pack/vim-javascript/start/vim-javascript
