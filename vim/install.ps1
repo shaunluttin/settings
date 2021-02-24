0e2e7daae79fd32c65178ebb05f3cb5959be7fd0
Shaun Luttin <shaun.luttin@mediavalet.com>
2021-02-23 16:38:04 -0800

    Update Vim settings

--- vim/install.ps1
+++ vim/install.ps1
@@ -1,2 +1,11 @@
 
+# Install Settings
 Copy-Item _vimrc ~\_vimrc
+
+# Install Bundles
+git clone https://github.com/prettier/vim-prettier ~/.vim/pack/plugins/start/vim-prettier
+git clone https://github.com/pangloss/vim-javascript.git ~/.vim/pack/vim-javascript/start/vim-javascript
+git clone https://github.com/leafgarland/typescript-vim.git ~/.vim/pack/typescript/start/typescript-vim
+
+# TODO Install preferred colour scheme.
+# TODO Install a completion bundle like CoC or YouCompleteMe.
