844360b1ec6054cb3220e67b9356a49ba7e7c2aa
Shaun Luttin <shaun.luttin@mediavalet.com>
2021-03-22 12:13:32 -0700

    Install extensions too

--- vscode/Install-VsCodeSettings.ps1
+++ vscode/Install-VsCodeSettings.ps1
@@ -2,6 +2,11 @@
 Copy-Item settings.json "$env:APPDATA\Code\User\"
 Copy-Item keybindings.json "$env:APPDATA\Code\User\"
 
+code --install-extension ms-dotnettools.csharp
+code --install-extension vscodevim.vim
+code --install-extension esbenp.prettier-vscode
+
+
 # Code Insiders
 # Copy-Item settings.json "$env:APPDATA\Code - Insiders\User\"
 # Copy-Item keybindings.json "$env:APPDATA\Code - Insiders\User\"
