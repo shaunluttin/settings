1dd3489eb55b1d3b27103a6c3d2e053fac894a09
Shaun Luttin <shaun.luttin@mediavalet.com>
2021-03-22 12:09:47 -0700

    Introduce PS1 scripts

---
+++ vscode/Install-VsCodeSettings.ps1
@@ -0,0 +1,8 @@
+# Code
+Copy-Item settings.json "$env:APPDATA\Code\User\"
+Copy-Item keybindings.json "$env:APPDATA\Code\User\"
+
+# Code Insiders
+# Copy-Item settings.json "$env:APPDATA\Code - Insiders\User\"
+# Copy-Item keybindings.json "$env:APPDATA\Code - Insiders\User\"
+
---
+++ vscode/Update-VsCodeSettings.ps1
@@ -0,0 +1,8 @@
+# Code
+Copy-Item "$env:APPDATA\Code\User\settings.json" .
+Copy-Item "$env:APPDATA\Code\User\keybindings.json" .
+
+# Code - Insiders
+# Copy-Item "$env:APPDATA\Code - Insiders\User\settings.json" .
+# Copy-Item "$env:APPDATA\Code - Insiders\User\keybindings.json" .
+
