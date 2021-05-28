2d5c5623f7522c68b65453958e614c559ced2301
Shaun Luttin <admin@shaunluttin.com>
2021-05-28 16:19:15 -0700

    Resolve-Path for bundle installation

--- vim/Install-Settings.ps1
+++ vim/Install-Settings.ps1
@@ -2,10 +2,15 @@
 # Install Settings
 Copy-Item $PSScriptRoot\_vimrc ~\_vimrc
 
+$path = Resolve-Path ~/
+
+Write-Host "Installing bundles to $path";
+
 # Install Bundles
-git clone https://github.com/prettier/vim-prettier ~/.vim/pack/plugins/start/vim-prettier
-git clone https://github.com/pangloss/vim-javascript.git ~/.vim/pack/vim-javascript/start/vim-javascript
-git clone https://github.com/leafgarland/typescript-vim.git ~/.vim/pack/typescript/start/typescript-vim
+git clone https://github.com/prettier/vim-prettier $path/.vim/pack/plugins/start/vim-prettier
+git clone https://github.com/pangloss/vim-javascript.git $path/.vim/pack/vim-javascript/start/vim-javascript
+git clone https://github.com/leafgarland/typescript-vim.git $path/.vim/pack/typescript/start/typescript-vim
+git clone https://github.com/PProvost/vim-ps1.git $path/.vim/pack/vim-ps1/start/vim-ps1
 
 # TODO Install preferred colour scheme.
 # TODO Install a completion bundle like CoC or YouCompleteMe.
