037670e040cb4b89331d9f5532bd8e2789f8e05d
Shaun Luttin <shaun.luttin@mediavalet.com>
2022-07-10 17:57:59 -0700

    Configure COC

--- vim/Install-Settings.ps1
+++ vim/Install-Settings.ps1
@@ -9,8 +9,8 @@ Copy-Item $PSScriptRoot\_vimrc ~\_vimrc;
 Write-Host "Installing bundles to $vimDir";
 
 # Install bundles
-git clone https://github.com/neoclide/coc.nvim.git $vimDir/pack/coc/plugins/start/coc
-git clone https://github.com/prettier/vim-prettier $vimDir/pack/vim-prettier/plugins/start/vim-prettier
+git clone https://github.com/neoclide/coc.nvim.git $vimDir/pack/coc/start/coc
+git clone https://github.com/prettier/vim-prettier $vimDir/pack/vim-prettier/start/vim-prettier
 git clone https://github.com/pangloss/vim-javascript.git $vimDir/pack/vim-javascript/start/vim-javascript
 git clone https://github.com/leafgarland/typescript-vim.git $vimDir/pack/typescript-vim/start/typescript-vim
 git clone https://github.com/PProvost/vim-ps1.git $vimDir/pack/vim-ps1/start/vim-ps1
