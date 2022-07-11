01d3862e2f638ed7a2bacbf5e149f9bc2babc970
Shaun Luttin <shaun.luttin@mediavalet.com>
2022-07-10 17:37:50 -0700

    Include COC

--- vim/Install-Settings.ps1
+++ vim/Install-Settings.ps1
@@ -1,15 +1,18 @@
 
-# Install Settings
-Copy-Item $PSScriptRoot\_vimrc ~\_vimrc
+# Ensure ~/.vim exists
+New-Item -Type Directory ~/.vim -Force;
+$vimDir = Resolve-Path ~/.vim;
 
-$vimDir = Resolve-Path ~/.vim
+# Install settings
+Copy-Item $PSScriptRoot\_vimrc ~\_vimrc;
 
 Write-Host "Installing bundles to $vimDir";
 
-# Install Bundles
-git clone https://github.com/prettier/vim-prettier $vimDir/pack/plugins/start/vim-prettier
+# Install bundles
+git clone https://github.com/neoclide/coc.nvim.git $vimDir/pack/coc/plugins/start/coc
+git clone https://github.com/prettier/vim-prettier $vimDir/pack/vim-prettier/plugins/start/vim-prettier
 git clone https://github.com/pangloss/vim-javascript.git $vimDir/pack/vim-javascript/start/vim-javascript
-git clone https://github.com/leafgarland/typescript-vim.git $vimDir/pack/typescript/start/typescript-vim
+git clone https://github.com/leafgarland/typescript-vim.git $vimDir/pack/typescript-vim/start/typescript-vim
 git clone https://github.com/PProvost/vim-ps1.git $vimDir/pack/vim-ps1/start/vim-ps1
 git clone https://github.com/chrisbra/csv.vim.git $vimDir/pack/csv-vim/start/csv-vim
 
